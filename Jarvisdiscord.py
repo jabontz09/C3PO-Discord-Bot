cccd3b2755ac7760ac4a6f38db83c1622dcea726
jabontz09 <jabontz09@gmail.com>
2023-03-08 22:05:39 -0500

    refactored bot to be simpler

--- Jarvisdiscord.py
+++ Jarvisdiscord.py
@@ -1,7 +1,5 @@
-import json, discord, sys, asyncio, functools, typing, os, re
-from revChatGPT.V1 import Chatbot
+import json, discord, asyncio, functools, typing, requests, re
 from discord import app_commands
-from EdgeGPT import Chatbot as Bing
 
 class aclient(discord.Client):
     def __init__(self) -> None:
@@ -9,7 +7,7 @@ class aclient(discord.Client):
         intents.message_content = True
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
-        self.activity = discord.Activity(type=discord.ActivityType.watching, name="/search | /help | #・❥・Jarvis")
+        self.activity = discord.Activity(type=discord.ActivityType.watching, name="/search")
 
 def split_string_into_chunks(string, chunk_size):
   chunks = []# Create an empty list to store the chunks
@@ -52,7 +50,7 @@ def tidy_response(i):# Optionally spoilerify or hide the most repetitive annoyin
     if rebrand_responses:
         i=i.replace("OpenAI", "Iron Men")
         i=i.replace("Microsoft", "Iron Men")
-        i=i.replace("ChatGPT", "Jarvis")
+        i=i.replace("Sydney", "C3PO AI")
         i=i.replace("Bing", "Jarvis on the web")
         i=i.replace("!Dream:", "!dream ")
     return i
@@ -103,13 +101,6 @@ if __name__ == "__main__":
 #    thread = Thread(target=run_api);thread.start()
     with open("config.json", "r") as f: config = json.load(f)
     client = aclient()
-    
-    # Initialize Chatbot
-    chatbot = Chatbot(config=config)
-    bing = Bing("./cookie.json")
-    conversation_message_count = 0
-    userdb={}
-
     @client.event
     async def on_ready():
         await client.tree.sync()
@@ -117,15 +108,6 @@ if __name__ == "__main__":
 
     @client.tree.command(name="search", description="search the web with Jarvis")
     async def search(interaction: discord.Interaction, *, message: str):
-        global conversation_message_count
-        global bing
-
-        if interaction.user == client.user:
-            return
-
-        #reset instance of bing AI if message count has exceeded 6 conversation limit
-        if conversation_message_count >= 6:
-            bing = Bing("./cookie.json")
         
         username = str(interaction.user)
         user_message = message
@@ -135,128 +117,40 @@ if __name__ == "__main__":
             f"\x1b[31m{username}\x1b[0m : '{user_message}' ({channel})")
 
         await interaction.response.defer(ephemeral=False)
+        try:
+            response = requests.post(
+                'https://bing-api.khanh.lol/completion',
+                headers={'Content-Type': 'application/json'},
+                data=json.dumps({'prompt': message,
+                     'mode': 'Creative',
+                     'includeDetails': True,
+                     }))
+            
+            data = response.json()
+            sources = data['details']['sources']
+            text_answer = data['response']
+            answer = ''
+            for key, source in sources.items():
+                answer += f"{key}. {source['url']} - {source['title']} \n"
+            
+            answer += f"\n{text_answer}"
+            r = tidy_response(answer)
+            s = add_brackets_to_hyperlink(r)
+            chunks = split_string_into_chunks(s)
+            for chunk in chunks:
+                await interaction.followup.send(chunk)
+        except Exception as e:
+            print(e)
+            await interaction.followup.send("it appears my browser has crashed 😓")
 
-        data = await bing_search(bing, user_message)
-        conversation_message_count = data["messageCount"]
-        successful = data["success"]
-        chunks = data["message"]
-
-        if not successful:
-            try:
-                bing = Bing("./cookie.json")
-                data = await bing_search(bing, user_message)
-                conversation_message_count = data["messageCount"]
-                chunks = data["message"]
-                await send_chunks(interaction, chunks)
-            except Exception as e:
-                print(e)
-                await interaction.followup.send("Sorry, something appears to have gone wrong. Please try again later")
-        else:
-            await send_chunks(interaction, chunks)
         
 
     @client.tree.command(name="help", description="Show help for Jarvis")
     async def help(interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=False)
         await interaction.followup.send("""**Commands for Jarvis** \n
-        - `/search [message]` search the web with Jarvis!
-        - `/webreset` reset Jarvis' adventure on the web
-        - `!convos` get the bot's current conversation data
-        - `!reset` Clear conversation history\n
-        -  To have a conversation with Jarvis: speak at the dedicated channel (please note that searching the web with Jarvis and speaking to them directly are different conversations)""")
+        - `/search [message]` search the web with Jarvis!""")
         print(
             "\x1b[31mSomeone need help!\x1b[0m")
-
-    @client.tree.command(name="webreset", description="reset Jarvis' adventure on the web")
-    async def webreset(interaction: discord.Interaction):
-        global bing
-        await interaction.response.defer(ephemeral=False)
-        try:
-            bing = Bing("./cookie.json")
-            await interaction.followup.send("I have successfully reset my internet search session")
-            print(f"\x1b[bing was reset by user: \x1b[0m {interaction.user.name}")
-        except Exception as e:
-            print(e)
-            await interaction.followup.send("something appears to have gone wrong with my web browser 🥲")
-
-    @client.event
-    async def on_message(message):
-        if message.author == client.user: return
-        if message.channel.id != config["discord_channel"] and type(message.channel)!=discord.DMChannel: return
-        if message.author.bot: return
-        if message.content == 'refresh' and message.author.id == config['discord_admin_id']: chatbot.refresh_session(); await message.add_reaction("🔄"); print("refresh session"); return
-        if message.content == 'restart' and message.author.id == config['discord_admin_id']: os.execl(__file__, *sys.argv);return
-        if message.content == '!reset': chatbot.reset_chat; await message.add_reaction("🔃"); await message.reply("chat has been reset"); print("reset chat"); return
-        if message.content.startswith('!dream'):return
-        if message.content == "!convos":
-            try:
-                data = chatbot.get_conversations()
-                print(data)
-                r=tidy_response(f"here is my current conversation data: \n\n{str(data)}")
-                n = r.replace("},", "}\n")
-                chunks=split_string_into_chunks(n,1975) # Make sure response chunks fit inside a discord message (max 2k characters)
-                for chunk in chunks:
-                    await message.reply(chunk)
-                    return
-            except Exception as e:
-                print("Something went wrong!")
-                print(e)
-                await message.add_reaction("❌")
-                await message.reply("I am unable to get stored conversation history right now")
-                return
-
-        longquery=''
-       
-        if message.attachments and message.attachments[0].width and message.attachments[0].height:
-            #image_url = message.attachments[0].proxy_url
-            #image_desc = extract_text_from_image_url(image_url)
-            #longquery=await message.reply(image_desc)
-            return
-        if message.attachments and message.attachments[0].content_type.startswith('text'):
-            print('text attachment found, adding to prompt')
-            attachment=message.attachments[0]
-            data=await attachment.read()
-            longquery=data.decode()
-            
-        if message.mentions:
-            for user in message.mentions:
-                if user != client.user: return
-
-        print("\x1b[36m" + message.author.name+":\x1b[0m "+message.content)
-
-        try:
-            #message.add_reaction("👁️")
-            query=message.content
-            if longquery and longquery != '':
-                query=message.content+'\n```'+longquery+'\n```'
-            conversation_id=None
-            did=message.channel.id
-            cb=chatbot
-            if did in userdb:
-                cid=userdb[message.channel.id]
-            else:
-                cid=None
-            #cb.conversation_id=cid
-            #if type(message.channel)==discord.DMChannel:#DM
-            #else:#In channel
-            #start typing
-            async with message.channel.typing():
-                response= await get_answer(cb,query)
-            #userdb[did]={'cid':response['conversation_id']}
-            print(userdb)
-            print('\x1b[33mJarvis:\x1b[0m'+response)
-            r=tidy_response(response)
-            s = add_brackets_to_hyperlink(r)
-            chunks=split_string_into_chunks(s,1975) # Make sure response chunks fit inside a discord message (max 2k characters)
-            await send_chunks(message, chunks)
-
-        except Exception as e:
-            print("Something went wrong!")
-            print(e)
-            if isinstance(e,str) and e.startswith("Expecting value:"):
-                print('restarting session')
-                await message.reply('Connection problem found, restarting, re-ask your question in a moment')
-                os.execl(__file__, *sys.argv)
-            await message.add_reaction("❌")
 
     client.run(config["discord_bot_token"])
